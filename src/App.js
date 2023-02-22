e71479c8b8176a61f8e4ee007c7d76e5e04d773b
daryacoding <abdullahzadeh.darya@gmail.com>
2023-02-21 20:53:28 -0500

    created components and color palette

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header'
+import Banner from './components/Banner/Banner'
+import Nav from './components/Nav/Nav'
+import About from './components/About/About'
+import Services from './components/Services/Services'
+import Work from './components/Work/Work'
+import Contact from './components/Contact/Contact'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Banner />
+      <Nav />
+      <About />
+      <Services />
+      <Work />
+      <Contact />
     </div>
   );
 }
