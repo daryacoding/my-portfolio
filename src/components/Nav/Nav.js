994f998b43e3fc35f1e66f7e9786a6db11078b0a
daryacoding <abdullahzadeh.darya@gmail.com>
2023-02-23 20:46:24 -0500

    changed color palette

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Nav() {
     return (
         <div className='nav-bar'>
+            <Link to="/">Home</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/experience">Experience</Link>
         </div>
