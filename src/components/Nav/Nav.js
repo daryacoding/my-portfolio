7a54507739ddc5c6f9bba02fdb0b0dc1041df028
Darya <abdullahzadeh.darya@gmail.com>
2023-09-28 20:02:54 -0400

    expand navbar created

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Reorder } from '@mui/icons-material'
+import "./Nav.css"
 
 export default function Nav() {
+    const [expandNavbar, setExpandNavbar] = useState(false);
+
     return (
-        <div className='nav-bar'>
-            <Link to="/">Home</Link>
-            <Link to="/projects">Projects</Link>
-            <a href='https://docs.google.com/document/d/1S7BpdCiVfFoFWLIiyvcqzw68rkPQf40xXSph9saZiFA/edit?usp=sharing' target='_blank'>My Resume</a>
+        <div className='nav-bar' id={expandNavbar ? "open" : "close"}>
+            <div className='toggle-button'>
+                <button
+                    onClick={() => {
+                        setExpandNavbar((prev) => !prev);
+                    }}
+                >
+                    <Reorder />
+                </button>
+            </div>
+            <div className='links'>
+                <Link to="/">Home</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/experience">Experience</Link>
+                <a href='https://docs.google.com/document/d/1S7BpdCiVfFoFWLIiyvcqzw68rkPQf40xXSph9saZiFA/edit?usp=sharing' target='_blank'>My Resume</a>
+            </div>
         </div>
     )
 }
